83e96dc00092d513f4df3b170e49fc842cf6925c
Scott Morgan <MorganSN@cardiff.ac.uk>
2016-10-13 11:02:53 +0100

    Altered fib program slightly

--- programs/mistakes/m2_fib.m
+++ programs/mistakes/m2_fib.m
@@ -2,20 +2,20 @@ clear; %clears workspace
 
 % % % % % % % % % % % % % % % % % % % % % % % % % % %
 % This program prints the Fibonacci sequence for N  %
-% terms. N is defined on line 8.                    %
+% terms. N is defined on line 10.                    %
 %                                                   %
 % Scott Morgan 2016                                 %
 % % % % % % % % % % % % % % % % % % % % % % % % % % %
 
-N = 50;
+N = 10;
 
 % % % Optional % % % 
 F = zeros(1,N); %pre-allocate the size of the vector F for speed.
 % % % End Optional % % %
 
-F(1) = 1; F(2) = 1;
+F(1) = 1; F(2) = 1; %first two terms for iterations
 for j = 1:N 
-    F(j) = F(j-1) + F(j-2); 
+    F(j) = F(j-1) + F(j-2); %do F_k = F_{k-1} + F_{k-2}
 end
 
 % % % Optional % % % 
